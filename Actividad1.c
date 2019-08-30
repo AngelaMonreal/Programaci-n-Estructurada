9cbf3b36f27ee5620ece620f2128d3141b12ecc6
AngelaMonreal <angiiehappylol@gmail.com>
2019-08-30 08:04:32 -0500

    Revert "Modificacion Actividad 1 segunda operacion algebraica"
    This reverts commit 163c7123238af4fe174dcd721898e7755ed162ba.

--- Actividad1.c
+++ Actividad1.c
@@ -11,8 +11,6 @@ int main(int argc, char *argv[])
 	if(x>=0 && x<=10){
 		a= ((x-1)/4)-((x-5)/36);
 		printf("El resultado para la primera operacion algebraica es: %f\n", a);
-		b= (6)*(((x+1)/8)-(((2*x)-3)/16));
-		printf("El resultado para la segunda operacion algebraica es: %.2f\n", b);
 	}else{
 		printf("El numero que ingresaste no es valido, intentalo de nuevo\n");}
 
