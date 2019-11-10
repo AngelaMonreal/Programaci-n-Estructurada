01aad692edcebf89386afe356515ce1ff1329ea3
AngelaMonreal <angiiehappylol@gmail.com>
2019-11-09 23:58:33 -0600

    Practica 14

--- main.c
+++ main.c
@@ -1,8 +1,60 @@
+/*
+*Autor: Angela Monreal
+*Fecha: 09/11/2019
+*Grupo: #006
+*Horario: 7:00 a.m - 9:00 a.m
+*Matricula: 1848122
+*/
+
+
 #include <stdio.h>
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+
 int main(int argc, char *argv[]) {
+//**********************************ACTIVIDAD 1*********************************
+/*#define cant 20
+struct picture_info{
+	char name[cant];
+	int date_time;
+} pictures [3]; 
+
+
+
+int n;
+pictures *ppicture;
+
+void leer(pictures *lista, int n)
+{
+int i,j;
+	for(i = 0; i <n; i++)
+	{
+	printf("alumno %d\n",i)
+	printf("Nombre: ");gets(lista[i].nombre);
+	printf("Matricula:");
+	scanf("%d",&lista[i].matricula);
+		for(j = 0; j<5; j++){
+		printf("Parcial %d: ",j);
+		scanf("%d",&lista[i].parcial[j]);
+		}
+	}
+}
+
+printf("Proporcione el numero de estudiantes: ");
+scanf("%d",&n);
+ppicture = (pictures *)malloc(n*sizeof(pictures));
+leer(ppicture, n);
+
+
+*/
+
+//**********************************ACTIVIDAD 2*********************************
+//¿Porque?
+//R: Porque se utilizan metodologias que deben realizar procesos que implican manejo de datos
+//¿Crees que utilizando punteros puedes ofrecer una alternativa más eficiente que evite reordenar los datos continuamente? 
+//R:Si, Debido aque se puede hacer el movimiento con la iteraccion de direcciones y su contenido para mayor eficiencia.
+
 	return 0;
 }
